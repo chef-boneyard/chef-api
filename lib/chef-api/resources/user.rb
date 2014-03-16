65901031a95d60b7eb2f4770285402eb1adc099c
Seth Vargo <sethvargo@gmail.com>
2014-03-16 18:41:14 -0400

    Add HEC endpoint for authenticating users

--- lib/chef-api/resources/user.rb
+++ lib/chef-api/resources/user.rb
@@ -7,5 +7,29 @@ module ChefAPI
       attribute :admin,       type: Boolean, default: false
       attribute :public_key,  type: String
     end
+
+    class << self
+      #
+      # Authenticate a user with the given +username+ and +password+.
+      #
+      # @example Authenticate a user
+      #   User.authenticate(username: 'user', password: 'pass')
+      #     #=> { "status" => "linked", "user" => { ... } }
+      #
+      # @param [Hash] options
+      #   the list of options to authenticate with
+      #
+      # @option options [String] username
+      #   the username to authenticate with
+      # @option options [String] password
+      #   the plain-text password to authenticate with
+      #
+      # @return [Hash]
+      #   the parsed JSON response from the server
+      #
+      def authenticate(options = {})
+        connection.post('/authenticate_user', options)
+      end
+    end
   end
 end
