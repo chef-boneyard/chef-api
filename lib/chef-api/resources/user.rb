abe83aac699034792a7145e7bbad7e6832908b2e
Seth Vargo <sethvargo@gmail.com>
2014-03-18 21:40:36 -0400

    User name => username

--- lib/chef-api/resources/user.rb
+++ lib/chef-api/resources/user.rb
@@ -3,9 +3,9 @@ module ChefAPI
     collection_path '/users'
 
     schema do
-      attribute :name,        type: String,  primary: true, required: true
-      attribute :admin,       type: Boolean, default: false
-      attribute :public_key,  type: String
+      attribute :username,   type: String,  primary: true, required: true
+      attribute :admin,      type: Boolean, default: false
+      attribute :public_key, type: String
     end
 
     class << self
